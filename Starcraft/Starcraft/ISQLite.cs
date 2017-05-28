9a4c37b14fda36199c6f0c9f1d3433cc6e670af7
Russell Chase <russellmchase@gmail.com>
2017-05-27 20:41:42 -0500

    added some sqlite dependencies and data models

---
+++ Starcraft/Starcraft/ISQLite.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using SQLite.Net;
+
+namespace Starcraft
+{
+    public interface ISQLite
+    {
+        SQLiteConnection GetConnection();
+    }
+}
---
+++ Starcraft/Starcraft/Strategy.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using SQLite.Net.Attributes;
+
+namespace Starcraft
+{
+    public class Strategy
+    {
+        [PrimaryKey, AutoIncrement]
+        public int ID { get; set; }
+        public string matchup { get; set; }
+        public string title { get; set; }
+        public string author { get; set; }
+        public string body { get; set; }
+        public DateTime CreatedOn { get; set; }
+
+        public Strategy()
+        {
+        }
+    }
+}
